bfb24428a4873612297112669c5aeb582c74698e
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-09 21:35:32 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,20 @@
 /**
  * main - Entry point
  *
- *
  * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	char c;
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		putchar(c);
-	}
-	for (c = 'A' ; c <= 'Z' ; c++)
-	{
-	putchar(c);
-	}
-	putchar('\n');
-	return (0);
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+for (c = 'A' ; c <= 'Z' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
