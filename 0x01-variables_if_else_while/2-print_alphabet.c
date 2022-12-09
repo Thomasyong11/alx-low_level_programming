c8df6a33a0de016acc97aee21bc4abaa4db0f7ac
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-09 13:39:32 -0800

    ty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@
  */
 int main(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
