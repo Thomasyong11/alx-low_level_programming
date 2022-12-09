cd5fbf8bb95493042f2b4a307fa00e7e043e3737
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-09 22:01:44 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-/**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n');
-return (0);
-}
+
