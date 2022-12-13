8ec6d4bd1a3ba1123d79992d4ea0bf7a27cca6ea
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-13 14:33:52 -0800

    tsk 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,9 +13,13 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('-');
+		_putchar('0');
 		return (-1);
 	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	else
 	{
 		_putchar('-');
