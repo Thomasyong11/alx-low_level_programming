44270beecacc8eb3e6c5e4cbe82424871fae6d9a
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-14 12:47:45 -0800

    nn

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.h"
 /**
  * main - Prints _putchar as a message.
@@ -16,3 +17,23 @@ int main(void)
 
 	return (0);
 }
+=======
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+		return (0);
+}
+>>>>>>> 5684746eab8824f5b23e15df20d9d265cdc79a7e
