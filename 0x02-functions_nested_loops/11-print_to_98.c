ef1c35a6748cf57d75afd98f9ff393c0dc4180a1
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-14 13:04:25 -0800

    sada

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,6 +20,18 @@ void print_to_98(int n)
 			n++;
 		}
 	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n)
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
 	else
 	{
 		printf("98");
