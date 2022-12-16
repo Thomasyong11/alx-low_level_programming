dd4d103fb6ab7de5f8d60dc93ebea1af48267dc1
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-16 21:27:36 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,19 +8,19 @@
   */
 void print_line(int n)
 {
-	   int x;
+	int x;
 
-   	if (n <= 0)
-	   {
-		     _putchar('\n');
-   	}
-   	else
-   	{
-		      for (x = 0; x < n; x++)
-	      	{
-		         	_putchar(95);
-		      }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
 
-		      _putchar('\n');
-	   }
+		_putchar('\n');
+	}
 }
