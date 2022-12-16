18fe4ce8208d43b9f54c2e4d4c5b485edcee4c42
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-16 21:04:06 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 /**
  * main - prints the numbers from 1 to 100
  * 3 multiples print Fizz instead of the number
-  * 5 multiples print Buzz instead of the number
+ * 5 multiples print Buzz instead of the number
  * 3 & 5 multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
