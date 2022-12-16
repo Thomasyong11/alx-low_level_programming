371b9cbf356f4a84e9e1800226c5a6ea42a21d47
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-16 20:40:56 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,20 @@
  * _putchar only 3 times
  * Return: 0-14 x10 followed by new line
  */
+ 
 void more_numbers(void)
 {
-	int i, ro;
-
-	for (ro = 0; ro < 10; ro++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i, ro;
+for (ro = 0; ro < 10; ro++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i >= 10)
+{
+_putchar((i / 10) + '0');
+}
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
+}
 }
