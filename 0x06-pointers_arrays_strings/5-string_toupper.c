f2661e0a1e6a287cbaf667e02d8bf687ac849fb7
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 04:56:15 -0800

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - change a string to upper case
+ *
+ * @str: pointer to a string
+ *
+ * Return: s
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
