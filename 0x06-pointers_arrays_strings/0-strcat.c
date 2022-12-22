1041ed13684630697262d1127b4b432569dcddb2
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 01:16:08 -0800

    ss

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,10 +13,10 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++)
 
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i] = src[j];
-			i++;
-		}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
 	return (dest);
 }
