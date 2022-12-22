fadfa339d6ad751bcb3d37f392896afd5715b429
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 09:24:59 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,18 +12,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	      int index, count;
+	int index, count;
 
-	      for (index = 0; dest[index] != '\0'; index++)
-		            ;
-	      for (count = 0; count < n && src[count] != '\0'; count++)
-	      {
-		            dest[index] = src[count];
-		            index++;
-	      }
-	      if (count < n)
-	      {
-	      dest[index] = '\0';
+	for (index = 0; dest[index] != '\0'; index++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[index] = src[count];
+		index++;
+	}
+	if (count < n)
+	{
+	 dest[index] = '\0';
       	}
-	      return (dest);
+	return (dest);
 }
