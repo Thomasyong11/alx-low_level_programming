764078a5f1f335cbc4086569ae340e7d3b3d3b4c
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 11:46:23 -0800

    task1 correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,11 @@
 #include "main.h"
 /**
- * _strncat - concatinate the values of two integers.
- *
- * @dest: arg a
- *
- * @src: arg b
- *
- * @n: arg n
- *
- * Return: void.
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
