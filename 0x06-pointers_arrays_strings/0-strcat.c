fcf4f894b8e3558f842c9993a5aa184da87cfbd1
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 01:10:27 -0800

    task0 correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,13 @@
  *  @dest: copy to
  *  @src: copy from
  *  Return: two concat strings
- */
+*/
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		;
+
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
