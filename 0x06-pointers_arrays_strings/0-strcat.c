a5a5111bd48fdf5fb6458edb0d64fcf33b53dc20
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2022-12-22 09:17:27 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,22 @@
  * Return: This return to dest, that concatenates two strings
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int count = 0, count2 = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-
-	for (j = 0; src[j] != '\0'; j++)
+	while (*(dest + count) != '\0')
 	{
-		dest[i] = src[j];
-		i++;
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
 	}
 	return (dest);
 }
