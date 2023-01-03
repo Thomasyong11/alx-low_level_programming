8464142216b03b97e97bfdad5ce21bd524961db8
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-03 12:35:04 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area,
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
- * Return: the pointer to dest.
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -14,5 +14,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
-	return(dest);
+	return (dest);
 }
