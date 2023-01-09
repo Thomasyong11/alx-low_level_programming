f7cc9b2f123bebb43b79239c86ff05bb4a6a2527
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 06:58:12 -0800

    ss

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
  * _strchr - locates a character in a string
- * @s: pointer where we search for charachter
- * @c: character we search for 
- * Return: NULL if character is not found, return pointer
  */
 char *_strchr(char *s, char c)
 {
