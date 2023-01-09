5879874c95c0a741149fc026143b0fd9139cf4e4
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 15:50:19 +0000

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,29 +1,14 @@
-nclude "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _isdigit - check for a digit (0 through 9)
- *
- *   * @c: digit checked
- *
- *    * Return: 1 if c is a digit returns 0 otherwise
- *
- *     */
-
-
+ * _isdigit - check for a digit (0 through 9)
+ * @c: digit checked
+ * Return: 1 if c is a digit returns 0 otherwise
+ */
 
 int _isdigit(int c)
-
 {
-
-	if (c >= '0' && c <= '9')
-
-		return (1);
-
-	return (0);
-
+if (c >= '0' && c <= '9')
+return (1);
+return (0);
 }
-
-
