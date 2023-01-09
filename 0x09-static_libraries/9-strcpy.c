48fe92d2e810383c2822c520770347e240953eeb
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 15:40:58 +0000

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,42 +1,21 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * *_strcpy - copy the string pointed by src, to the buffer pointed to by dest
- *
- *   * @dest: char pointer
- *
- *    * @src: char pointer 2
- *
- *     * Return: dest pointer
- *
- *      */
-
-
+ * *_strcpy - copy the string pointed by src, to the buffer pointed to by dest
+ * @dest: char pointer
+ * @src: char pointer 2
+ * Return: dest pointer
+ */
 
 char *_strcpy(char *dest, char *src)
-
 {
+	int len, count;
 
-		int len, count;
+	len = 0;
 
-
-
-			len = 0;
-
-
-
-				while (src[len] != '\0')
-
-							len++;
-
-					for (count = 0; count <= len; count++)
-
-								dest[count] = src[count];
-
-						return (dest);
-
+	while (src[len] != '\0')
+		len++;
+	for (count = 0; count <= len; count++)
+		dest[count] = src[count];
+	return (dest);
 }
-
