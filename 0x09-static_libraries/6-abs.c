922c1bd9cbed452b0b0a4aabdeed58bd8113981a
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 15:40:16 +0000

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,27 +1,14 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _abs - determine the absolute value of an integer
- *
- *   * @i: variable to determine the absolute value of an integer
- *
- *    * Return: i if succesful
- *
- *     */
-
-
+ * _abs - determine the absolute value of an integer
+ * @i: variable to determine the absolute value of an integer
+ * Return: i if succesful
+ */
 
 int _abs(int i)
-
 {
-
-	if (i < 0)
-
-		i *= -1;
-
-	return (i);
-
+if (i < 0)
+i *= -1;
+return (i);
 }
