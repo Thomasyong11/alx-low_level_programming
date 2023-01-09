408a8fefa12b6024e0dd371b4d2d7a08ca7c4129
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 07:01:58 -0800

    crt2

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,19 +2,31 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string
- * Return: the length of a string
+ *  @s: string
+ *  Return: the length of a string
  */
-
 int _strlen(char *s)
-{
-	int i;
 
-	i = 0;
-	while (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-	return (i);
+{
+
+		int longi = 0;
+
+
+
+			while (*s != '\0')
+
+					{
+
+								longi++;
+
+										s++;
+
+											}
+
+
+
+				return (longi);
+
 }
+
+
