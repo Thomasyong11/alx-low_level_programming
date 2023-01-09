c211d70a0afa6fa7f5ada5d236a564b7727a222f
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-09 06:04:23 -0800

    update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
+ *  _strcat - concatenates two strings
  * @dest: first string to be added to
  * @src: second string to be added
  * Return: concatenated string
@@ -9,7 +9,8 @@ char *_strcat(char *dest, char *src)
 {
 	char *temp = dest;
 
-	for (; *temp != '\0'; temp++);
+	for (; *temp != '\0'; temp++)
+		;
 	for (; *src != '\0'; src++)
 	{
 		*temp = *src;
