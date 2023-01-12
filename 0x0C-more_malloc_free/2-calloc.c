c6fdb250535598feb234d11910925da9c8d2a6c0
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-12 13:21:45 +0000

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,59 +1,29 @@
-nclude "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 /**
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
  *
- *  * _calloc - allocates memory for an array.
- *
- *   * @nmemb: number of elements.
- *
- *    * @size: size of bytes.
- *
- *     *
- *
- *      * Return: pointer to the allocated memory.
- *
- *       * if nmemb or size is 0, returns NULL.
- *
- *        * if malloc fails, returns NULL.
- *
- *         */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
+ */
+int *array_range(int min, int max)
 {
+	int *ar;
+	int i;
 
-		char *p;
+	if (min > max)
+		return (NULL);
 
-			unsigned int i;
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
+	if (ar == NULL)
+		return (NULL);
 
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
 
-				if (nmemb == 0 || size == 0)
-
-							return (NULL);
-
-
-
-					p = malloc(nmemb * size);
-
-
-
-						if (p == NULL)
-
-									return (NULL);
-
-
-
-							for (i = 0; i < (nmemb * size); i++)
-
-										p[i] = 0;
-
-
-
-								return (p);
-
+	return (ar);
 }
-
-
