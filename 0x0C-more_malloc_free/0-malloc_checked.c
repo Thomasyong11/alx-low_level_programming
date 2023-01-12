43115534ac6afd57c52cce47c9d6fb55275f0127
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-12 13:09:39 +0000

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,38 +1,24 @@
-nclude "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 #include <stdio.h>
 
-
-
 /**
- *
- *   * malloc_checked - Allocates memory
- *
- *     * @b: the size to allocate
- *
- *       *
- *
- *         * Return: Nothing.
- *
- *           */
-
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: Nothing.
+  */
 void *malloc_checked(unsigned int b)
-
 {
+	void *p;
 
-		void *p;
+	p = malloc(b);
 
+	if (p == NULL)
+		exit(98);
 
-
-			p = malloc(b);
-
-
-
-				if (p == NULL)
-
-							exit(98);
+	return (p);
+}
 
 
 
