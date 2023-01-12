488825302cb7f24e6a1aeb1b78cd6da8523b8fb5
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-12 05:08:20 -0800

    created initial files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,41 @@
+nclude "holberton.h"
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *   * malloc_checked - Allocates memory
+ *
+ *     * @b: the size to allocate
+ *
+ *       *
+ *
+ *         * Return: Nothing.
+ *
+ *           */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+		void *p;
+
+
+
+			p = malloc(b);
+
+
+
+				if (p == NULL)
+
+							exit(98);
+
+
+
+					return (p);
+
+}
