274932d72c7eb03aae432722b89bb9c0da5fe603
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-26 19:39:47 +0000

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,45 +1,19 @@
 #include "lists.h"
-
 /**
- *
- *  * print_list - prints all the elements of a list_t list.
- *
- *   * @h: singly linked list.
- *
- *    * Return: number of elements in the list.
- *
- *     */
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
 
-
-
-size_t print_list(const list_t *h)
-
+size_t list_len(const list_t *h)
 {
+	size_t nelem;
 
-		size_t nelem;
-
-
-
-			nelem = 0;
-
-				while (h != NULL)
-
-						{
-
-									if (h->str == NULL)
-
-													printf("[%d] %s\n", 0, "(nil)");
-
-											else
-
-															printf("[%d] %s\n", h->len, h->str);
-
-													h = h->next;
-
-															nelem++;
-
-																}
-
-					return (nelem);
-
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
