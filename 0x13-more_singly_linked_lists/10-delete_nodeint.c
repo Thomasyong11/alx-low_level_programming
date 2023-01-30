6a7dfff9bcb1518d1c2fc252512e5b403410b652
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-30 20:05:29 +0000

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,43 +1,47 @@
 #include "lists.h"
 
-
-
 /**
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
  *
- *  * sum_listint - returns the sum of all the data (n) of
- *
- *   * a linked list.
- *
- *    * @head: head of a list.
- *
- *     *
- *
- *      * Return: sum of all the data (n).
- *
- *       */
-
-int sum_listint(listint_t *head)
-
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-		int sum;
+	prev = *head;
 
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
 
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
 
-			sum = 0;
+	next = prev->next;
 
-				while (head != NULL)
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
 
-						{
-
-									sum += head->n;
-
-											head = head->next;
-
-												}
-
-
-
-					return (sum);
-
+	return (1);
 }
