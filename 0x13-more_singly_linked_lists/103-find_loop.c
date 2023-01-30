5c2ccde9f11ce68aa9fd0c66a89fc65c8d1135d3
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-01-30 20:13:47 +0000

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,53 +1,42 @@
- #include "lists.h"
-
-
+#include "lists.h"
 
 /**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: head of a list.
  *
- *  * reverse_listint - reverses a linked list.
- *
- *   * @head: head of a list.
- *
- *    *
- *
- *     * Return: pointer to the first node.
- *
- *      */
-
-listint_t *reverse_listint(listint_t **head)
-
+ * Return: the address of the node where the loop starts.
+ */
+listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *p2;
+	listint_t *prev;
 
-		listint_t *p;
+	p2 = head;
+	prev = head;
+	while (head && p2 && p2->next)
+	{
+		head = head->next;
+		p2 = p2->next->next;
 
-			listint_t *n;
+		if (head == p2)
+		{
+			head = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != head && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == head)
+					break;
 
+				head = head->next;
+			}
+			return (p2->next);
+		}
+	}
 
-
-				p = NULL;
-
-					n = NULL;
-
-
-
-						while (*head != NULL)
-
-								{
-
-											n = (*head)->next;
-
-													(*head)->next = p;
-
-															p = *head;
-
-																	*head = n;
-
-																		}
-
-
-
-							*head = p;
-
-								return (*head);
-
+	return (NULL);
 }
