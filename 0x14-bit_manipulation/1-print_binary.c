297fac8e987f4939fdac3101c25729ff04415955
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-02-02 19:39:02 +0000

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,73 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
  *
- *  * binary_to_uint - converts a binary number to an
- *
- *   * unsigned int.
- *
- *    * @b: binary.
- *
- *     *
- *
- *      * Return: unsigned int.
- *
- *       */
-
-unsigned int binary_to_uint(const char *b)
-
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
 {
-
-		unsigned int ui;
-
-			int len, base_two;
-
-
-
-				if (!b)
-
-							return (0);
-
-
-
-					ui = 0;
-
-
-
-						for (len = 0; b[len] != '\0'; len++)
-
-									;
-
-
-
-							for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-
-									{
-
-												if (b[len] != '0' && b[len] != '1')
-
-															{
-
-																			return (0);
-
-																					}
-
-
-
-														if (b[len] & 1)
-
-																	{
-
-																					ui += base_two;
-
-																							}
-
-															}
-
-
-
-								return (ui);
-
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
