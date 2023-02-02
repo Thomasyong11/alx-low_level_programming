9e2af89d8bcfdeae9f02c536899bb317eed9f2a8
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-02-02 19:39:43 +0000

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,73 +1,27 @@
 #include "holberton.h"
 
-
-
 /**
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
  *
- *  * binary_to_uint - converts a binary number to an
- *
- *   * unsigned int.
- *
- *    * @b: binary.
- *
- *     *
- *
- *      * Return: unsigned int.
- *
- *       */
-
-unsigned int binary_to_uint(const char *b)
-
+ * Return: value of the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i;
 
-		unsigned int ui;
+	if (n == 0 && index < 64)
+		return (0);
 
-			int len, base_two;
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
-
-
-				if (!b)
-
-							return (0);
-
-
-
-					ui = 0;
-
-
-
-						for (len = 0; b[len] != '\0'; len++)
-
-									;
-
-
-
-							for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-
-									{
-
-												if (b[len] != '0' && b[len] != '1')
-
-															{
-
-																			return (0);
-
-																					}
-
-
-
-														if (b[len] & 1)
-
-																	{
-
-																					ui += base_two;
-
-																							}
-
-															}
-
-
-
-								return (ui);
-
+	return (-1);
 }
